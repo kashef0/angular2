0be27c16c40bfe6e0af396e6196a4424ebaba58f
M.kashef <kashef.sy@gmail.com>
2024-04-26 22:23:02 +0200

    skapade angular app med services, interface och courses_list komponenter som sortera och filtrera data om kurs från api

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CourseListComponent } from './course-list/course-list.component';
+
+export const routes: Routes = [
+    {path: "course_list", component: CourseListComponent}
+];
---
+++ src/app/course-list/course-list.component.ts
@@ -0,0 +1,57 @@
+
+import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+
+
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './course-list.component.html',
+  styleUrl: './course-list.component.scss'
+})
+export class CourseListComponent {
+  
+
+  courses_list: Course[] = [];
+  filteringCourses: Course[] = [];
+  search: string = '';
+  constructor(private courseservice: CoursesService) {}
+
+  ngOnInit() {
+    this.courseservice.getCourses().subscribe(data => {
+      this.courses_list = data;
+      this.filteringCourses = [...this.courses_list];
+    });
+  }
+  
+  
+  sortBy(row: string) {
+    if (row == "code") {
+      this.filteringCourses = this.courses_list.sort((el, re) => el.code.localeCompare(re.code));
+    } else if (row == "coursename") {
+      this.filteringCourses = this.courses_list.sort((el, re) => el.coursename.localeCompare(re.coursename));
+    } else if (row == "progression") {
+      this.filteringCourses = this.courses_list.sort((el, re) => el.progression.localeCompare(re.progression));
+    } else if (row == "syllabus") {
+      this.filteringCourses = this.courses_list.sort((el, re) => el.coursename.localeCompare(re.coursename));
+    }
+  }
+  
+  onSubmit() {
+      console.log(this.search);
+      this.filteringCourses = this.courses_list.filter(course =>
+        course.code.includes(this.search.toLowerCase()) ||
+        course.coursename.toLowerCase().includes(this.search.toLowerCase())
+      
+    )}
+    
+  }
