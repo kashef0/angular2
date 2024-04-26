b70191354c0bb7f87b6d780768e3a0f2503d269b
M.kashef <kashef.sy@gmail.com>
2024-04-27 00:32:39 +0200

    fixade sökväg till courses_list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseListComponent } from "./course-list/course-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CourseListComponent]
 })
 export class AppComponent {
   title = 'courses';
